aa6fd380247ded57a7aec7255dce267cff1ee931
xabieralbizu2 <xabialar696@gmail.com>
2024-11-18 19:58:56 +0100

    Initialized project with wheel movement and car movement

---
+++ Assets/Scripts/CM.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CM : MonoBehaviour
+{
+    public Rigidbody rb;
+    public WheelCollider lfW,rfW,lbW,rbW;
+    public float driveSpeed, steerSpeed;
+    float hInput, vInput;
+    float mult;
+
+
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+    }
+    private void FixedUpdate()
+    {
+        ForwardMovement(vInput);
+
+        lfW.steerAngle = steerSpeed * hInput;
+        rfW.steerAngle = steerSpeed * hInput;
+
+    }
+
+    private void ForwardMovement(float vInput)
+    {
+        float motor = vInput * driveSpeed;
+
+        if (vInput > 0)
+        {
+            // Aplica motor torque
+            lfW.motorTorque = motor * Time.timeScale;
+            rfW.motorTorque = motor * Time.timeScale;
+            lbW.motorTorque = motor * Time.timeScale;
+            rbW.motorTorque = motor * Time.timeScale;
+        }
+        else
+        {
+            // Aplica brake torque (frenado)
+            float brakeTorque = Mathf.Abs(motor); // Usa la magnitud del motor torque para el frenado
+            lfW.brakeTorque = brakeTorque;
+            rfW.brakeTorque = brakeTorque;
+            lbW.brakeTorque = brakeTorque;
+            rbW.brakeTorque = brakeTorque;
+        }
+
+        Debug.Log(rbW.motorTorque);
+    }
+    
+
+
+}
